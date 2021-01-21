d944d6ee615e6728880aa73bded279e89007a4fe
Melquias Ribeiro <melquias10.mr@gmail.com>
2021-01-21 11:01:31 -0300

    feat: inital configs and structure

---
+++ src/controllers/UserController.js
@@ -0,0 +1,8 @@
+
+class PlantController {
+    async index(req, res) {
+     return res.json({"ok":"ta funcionando"})
+    }
+}
+
+export default new PlantController();
--- src/index.js
+++ src/index.js
@@ -1,69 +1,39 @@
-var http = require("http");
-var users = [
-  {
-    id: 1,
-    name: "João",
-    username: "joao",
-    email: "joao@gmail.com"
-  }
-];
-http
-  .createServer(function (req, res) {
-    var url = req.url;
-    var method = req.method;
-    res.setHeader("Content-Type", "application/json");
-    if (url.startsWith("/users")) {
-      if (method === "GET") {
-        res.write(JSON.stringify(users));
-      }
-      if (method === "POST") {
-        let body = "";
-        req.on("data", chunk => {
-          body += chunk.toString(); // convert Buffer to string
-        });
-        req.on("end", () => {
-          users.push(JSON.parse(body));
-          res.end("ok");
-          //res.write(JSON.stringify(users));
-        });
-      }
-      if (method === "PUT") {
-        let body = "";
-        req.on("data", chunk => {
-          body += chunk.toString(); // convert Buffer to string
-        });
-        req.on("end", () => {
-          var id = url.split("/")[2];
-          var user = users.find(el => {
-            return el.id === parseInt(id);
-          });
-          body = JSON.parse(body);
-          user.name = body.name;
-          user.username = body.username;
-          user.email = body.email;
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import Youch from 'youch';
+// eslint-disable-next-line import/no-unresolved
+import 'express-async-errors';
+import Routes from './routes';
+import './database';
 
-          res.end("ok");
-          //res.write(JSON.stringify(users));
-        });
-      }
-      if (method === "DELETE") {
-        var id = url.split("/")[2];
-        var index = users.findIndex(el => {
-          return el.id === parseInt(id);
-        });
-
-        users.splice(parseInt(index), 1);
-        if (index === -1) {
-          res.statusCode = 404;
-        }
-      }
-
-      res.end(); //end the response
-    } else {
-      res.write("<h1>Prova Backend Helpper<h1>"); //write a response
-      res.end(); //end the response
+class APP {
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+        this.exceptionHandler();
     }
-  })
-  .listen(3000, function () {
-    console.log("server start at port 3000"); //the server object listens on port 3000
-  });
+
+    middlewares() {
+        this.server.use(cors());
+        this.server.use(express.json());
+    }
+
+    routes() {
+        this.server.use(Routes);
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            if (process.env.NODE_ENV === 'development') {
+                const errors = await new Youch(err, req).toJSON();
+
+                return res.status(500).json(errors);
+            }
+            return res.status(500).json({ error: 'internal server error' });
+        });
+    }
+}
+
+export default new APP().server;
